bced71f4829cb79555086e497e1eb961bbd73099
Jenil Shah <131328441+Jenil9761@users.noreply.github.com>
2024-08-05 14:22:41 +0530

    Update Hero.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -18,7 +18,7 @@ const Hero = () => {
               type: "ease-in",
             }}
             >
-              Jenil <br />
+              Jenil 32<br />
               Most Suitable
               <br /> Property
             </motion.h1>
